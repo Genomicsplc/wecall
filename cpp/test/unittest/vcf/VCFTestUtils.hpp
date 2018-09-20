fd04608c79d1fe6f335b82e4ec7bee2b92e03e7f
stefanie <stefanie.thiem@genomicsplc.com>
2018-09-20 15:18:10 +0100

    remove non-existing function from hpp

--- cpp/test/unittest/vcf/VCFTestUtils.hpp
+++ cpp/test/unittest/vcf/VCFTestUtils.hpp
@@ -6,8 +6,6 @@
 
 using echidna::variant::varPtr_t;
 
-void check_equal_info( const echidna::vcf::Info & expected, const echidna::vcf::Info & parsed );
-
 boost::test_tools::predicate_result checkVariantInVector( const std::vector< varPtr_t > & variants,
                                                           const varPtr_t & testVariant );
 
